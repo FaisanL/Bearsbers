98b8f740fc4d2a62a0bac19ebec8eb1c9531260e
Faisan <fabian.m.lobospalma@gmail.com>
2024-12-06 21:20:00 -0300

    Añadiendo nuevos styles y contenido

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,26 @@
 <head>
     @vite('resources/css/home.css')
 </head>
+<div class="container cont-somos my-4 py-4 w-3/4 lg:w-1/2">
+    <h1 class="text-center uppercase">¿Quienes somos?</h1>
+    <p class="text-center text-wrap">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente quos
+        optio, voluptatibus vero aperiam officiis recusandae doloremque saepe
+        cumque libero incidunt delectus expedita blanditiis ipsa natus soluta
+        ducimus rerum cupiditate?
+    </p>
+</div>
+<div>
+    <h2 class="text-center uppercase">Un poco de nuestro trabajo</h2>
+
+    <!-- Un Carousel aqui -->
+</div>
 <div class="img-layout">
-    <div class="cont-layout flex justify-center">
+    <div class="cont-layout">
         <div class="cont-title">
-            <h1 class="text-center">Agenda tu Cita!</h1>
+            <button class="text-center">Agenda tu Cita!</button>
         </div>
     </div>
 </div>
+
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,51 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>@yield('title')</title>
-        @vite('resources/css/app.css')
-        <!-- Asegúrate de que este archivo está apuntando al correcto -->
-        @vite('resources/js/app.js')
-        <!-- Si estás usando JS, asegúrate de que también está incluido -->
-        <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-        />
-    </head>
 
-    <body>
-        <nav class="flex items-center justify-between cont-nav">
-            <div>
-                <img
-                    src="{{ asset('home/Bearsber.png') }}"
-                    alt="logo"
-                    class="img"
-                />
-            </div>
-            <ul class="flex justify-center items-center space-x-4 pr-4 m-0">
-                <li class="p-2 font-bold">
-                    <a class="link" href="/">Home</a>
-                </li>
-                <li class="p-2 font-bold">
-                    <a class="link" href="/citas">Citas</a>
-                </li>
-                <li class="p-2 font-bold">
-                    <a class="link" href="/contacto">Contacto</a>
-                </li>
-                <li class="p-2 font-bold">
-                    <a class="link" href="/nosotros">Nosotros</a>
-                </li>
-            </ul>
-        </nav>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>@yield('title')</title>
+    @vite('resources/css/app.css')
+    <!-- Asegúrate de que este archivo está apuntando al correcto -->
+    @vite('resources/js/app.js')
+    <!-- Si estás usando JS, asegúrate de que también está incluido -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Domine:wght@400..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
 
-        <!-- Contenido dinámico -->
-        @yield('content')
+    <style>
+        * {
+            font-family: "Domine", serif;
+            font-weight: 500;
+        }
+    </style>
+</head>
 
-        <footer class="cont-footer flex justify-center">
-            <p class="text-center text-gray-400">
-                Todos los derechos reservados &copy;
-            </p>
-        </footer>
-    </body>
+<body>
+    <nav class="flex items-center justify-between cont-nav">
+        <div>
+            <img src="{{ asset('home/Bearsber.png') }}" alt="logo" class="img" />
+        </div>
+        <ul class="flex justify-center items-center space-x-4 pr-4 m-0">
+            <li class="p-2 font-bold">
+                <a class="link" href="/">Home</a>
+            </li>
+            <li class="p-2 font-bold">
+                <a class="link" href="/citas">Citas</a>
+            </li>
+            <li class="p-2 font-bold">
+                <a class="link" href="/contacto">Contacto</a>
+            </li>
+            <li class="p-2 font-bold">
+                <a class="link" href="/nosotros">Nosotros</a>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- Contenido dinámico -->
+    @yield('content')
+
+    <footer class="cont-footer flex justify-center">
+        <p class="text-center text-gray-400">
+            Todos los derechos reservados &copy;
+        </p>
+    </footer>
+</body>
+
 </html>
