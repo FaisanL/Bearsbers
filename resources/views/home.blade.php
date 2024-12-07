e6015278c7589775b26a10c2b7faa9eb7ac5326b
Faisan <fabian.m.lobospalma@gmail.com>
2024-12-07 20:33:15 -0300

    Añadiendo carousel y estilos a home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,14 @@
 @extends('layout') @section('title', 'Inicio') @section('content')
 <head>
-    @vite('resources/css/home.css')
+    @vite('resources/css/home.css') @vite('resources/js/app.js')
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+        crossorigin="anonymous"
+    />
 </head>
-<div class="container cont-somos my-4 py-4 w-3/4 lg:w-1/2">
+<div class="container cont-somos my-8 py-4 w-3/4 lg:w-1/2">
     <h1 class="text-center uppercase">¿Quienes somos?</h1>
     <p class="text-center text-wrap">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente quos
@@ -11,17 +17,64 @@
         ducimus rerum cupiditate?
     </p>
 </div>
-<div>
+
+<div class="my-16">
     <h2 class="text-center uppercase">Un poco de nuestro trabajo</h2>
 
-    <!-- Un Carousel aqui -->
-</div>
-<div class="img-layout">
-    <div class="cont-layout">
-        <div class="cont-title">
-            <button class="text-center">Agenda tu Cita!</button>
+    <div id="carouselExample" class="carousel slide mx-auto w-3/4 lg:w-1/2">
+        <div class="carousel-inner">
+            <div class="carousel-item active cont-carousel">
+                <img
+                    src="{{ asset('home/ejemplo.png') }}"
+                    class="d-block w-100"
+                    alt="..."
+                />
+            </div>
+            <div class="carousel-item cont-carousel">
+                <img
+                    src="{{ asset('home/ejemplo.png') }}"
+                    class="d-block w-100"
+                    alt="..."
+                />
+            </div>
+            <div class="carousel-item cont-carousel">
+                <img
+                    src="{{ asset('home/ejemplo.png') }}"
+                    class="d-block w-100"
+                    alt="..."
+                />
+            </div>
         </div>
+        <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="prev"
+        >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="next"
+        >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
 </div>
 
+<div class="img-layout flex justify-center items-center">
+    <div class="cont-title">
+        <button class="text-center w-52 h-14">Agenda tu Cita!</button>
+    </div>
+</div>
+
+<script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"
+></script>
 @endsection
